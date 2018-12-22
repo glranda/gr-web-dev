abc2192a7f8d2c27641cdf953410c66f30477bb4
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 14:17:46 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -4,29 +4,12 @@ $(function() {
     .then(jsonOne => console.log(jsonOne))
 });
 
-/////////
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
-
-let newRow = '<div class="table__row"><div ondragstart="drag(event)" draggable="true" class="user-id1 table__cell table__cell--short"></div><div ondragstart="drag(event)" draggable="true" class="album-name1 table__cell"></div></div>';
-var containerOne = $(".table-one");
-var containerTwo = $(".table-two");
-
 $.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
+  var container = $(".table-one");
+
   for(var i = 0; i < data1.length; i++) {
-    containerOne.after(newRow);
+    let newRow = '<div class="table__row"><div draggable="true" class="user-id1 table__cell table__cell--short"></div><div draggable="true" class="album-name1 table__cell"></div></div>';
+    container.after(newRow);
   }
 
   let ids = $(".user-id1");
@@ -35,11 +18,15 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
     ids[j].innerHTML = data1[j].userId;
     albums[j].innerHTML = data1[j].title;
   }
+
 });
 
 $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
+  var container = $(".table-two");
+
   for(var i = 0; i < data2.length; i++) {
-    containerTwo.after(newRow);
+    let newRow = '<div class="table__row"><div class="user-id2 table__cell table__cell--short"></div><div class="album-name2 table__cell"></div></div>';
+    container.after(newRow);
   }
 
   let ids = $(".user-id2");
@@ -48,4 +35,5 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
     ids[j].innerHTML = data2[j].userId;
     albums[j].innerHTML = data2[j].title;
   }
+
 });
