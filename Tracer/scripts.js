b626301ee3bf238fd3a309a3ca5557fab9d9e93a
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 16:21:20 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -48,42 +48,6 @@ function isBefore(el1, el2) {
     return false;
 }
 
-////// -- Build and Populate rows
-
-$.get(userId1, function(data1) {
-  let container = $(".table-one");
-
-  for(var i = 0; i < data1.length; i++) {
-    let newRow = '<div draggable="true" ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)" class="table__row"><div class="user-id1 table__cell table__cell--short"></div><div class="album-name1 table__cell"></div></div>';
-    container.after(newRow);
-  }
-
-  let ids = $(".user-id1");
-  let albums = $(".album-name1");
-  for(var j = 0; j < albums.length; j++) {
-    ids[j].innerHTML = data1[j].userId;
-    albums[j].innerHTML = data1[j].title;
-  }
-
-});
-
-$.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
-  let container = $(".table-two");
-
-  for(var i = 0; i < data2.length; i++) {
-    let newRow = '<div draggable="true" ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)" class="table__row"><div class="user-id2 table__cell table__cell--short"></div><div class="album-name2 table__cell"></div></div>';
-    container.after(newRow);
-  }
-
-  let ids = $(".user-id2");
-  let albums = $(".album-name2");
-  for(var j = 0; j < albums.length; j++) {
-    ids[j].innerHTML = data2[j].userId;
-    albums[j].innerHTML = data2[j].title;
-  }
-
-});
-
 ////// -- User Select
 
 let selectUser1 = document.getElementById("select1");
@@ -100,3 +64,39 @@ function updateUserY() {
   y = newUser2;
   console.log(y);
 }
+
+////// -- Build and Populate rows
+
+$.get(x, function(data1) {
+  let container = $(".table-one");
+
+  for(var i = 0; i < data1.length; i++) {
+    let newRow = '<div draggable="true" ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)" class="table__row"><div class="user-id1 table__cell table__cell--short"></div><div class="album-name1 table__cell"></div></div>';
+    container.after(newRow);
+  }
+
+  let ids = $(".user-id1");
+  let albums = $(".album-name1");
+  for(var j = 0; j < albums.length; j++) {
+    ids[j].innerHTML = data1[j].userId;
+    albums[j].innerHTML = data1[j].title;
+  }
+
+});
+
+$.get(y, function(data2) {
+  let container = $(".table-two");
+
+  for(var i = 0; i < data2.length; i++) {
+    let newRow = '<div draggable="true" ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)" class="table__row"><div class="user-id2 table__cell table__cell--short"></div><div class="album-name2 table__cell"></div></div>';
+    container.after(newRow);
+  }
+
+  let ids = $(".user-id2");
+  let albums = $(".album-name2");
+  for(var j = 0; j < albums.length; j++) {
+    ids[j].innerHTML = data2[j].userId;
+    albums[j].innerHTML = data2[j].title;
+  }
+
+});
