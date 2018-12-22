d1a20c2001da44324f969f86e1a21c7465605c2d
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 14:15:16 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -4,12 +4,29 @@ $(function() {
     .then(jsonOne => console.log(jsonOne))
 });
 
-$.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
-  var container = $(".table-one");
+/////////
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+let newRow = '<div class="table__row"><div ondragstart="drag(event)" draggable="true" class="user-id1 table__cell table__cell--short"></div><div ondragstart="drag(event)" draggable="true" class="album-name1 table__cell"></div></div>';
+var containerOne = $(".table-one");
+var containerTwo = $(".table-two");
+
+$.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
   for(var i = 0; i < data1.length; i++) {
-    let newRow = '<div class="table__row"><div class="user-id1 table__cell table__cell--short"></div><div class="album-name1 table__cell"></div></div>';
-    container.after(newRow);
+    containerOne.after(newRow);
   }
 
   let ids = $(".user-id1");
@@ -18,15 +35,11 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
     ids[j].innerHTML = data1[j].userId;
     albums[j].innerHTML = data1[j].title;
   }
-
 });
 
 $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
-  var container = $(".table-two");
-
   for(var i = 0; i < data2.length; i++) {
-    let newRow = '<div class="table__row"><div class="user-id2 table__cell table__cell--short"></div><div class="album-name2 table__cell"></div></div>';
-    container.after(newRow);
+    containerTwo.after(newRow);
   }
 
   let ids = $(".user-id2");
@@ -35,5 +48,4 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
     ids[j].innerHTML = data2[j].userId;
     albums[j].innerHTML = data2[j].title;
   }
-
 });
