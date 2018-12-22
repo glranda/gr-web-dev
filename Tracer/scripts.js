c05a91036e13ef5170e7d2012fbaff825e7f1fe2
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 10:29:27 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,26 +1,44 @@
-// $(function() {
-//   fetch('https://jsonplaceholder.typicode.com/albums/1')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-// });
+$(function() {
+  fetch('https://jsonplaceholder.typicode.com/albums/1')
+    .then(responseOne => responseOne.json())
+    .then(jsonOne => console.log(jsonOne))
+
+    fetch('https://jsonplaceholder.typicode.com/albums/2')
+      .then(responseTwo => responseTwo.json())
+      .then(jsonTwo => console.log(jsonTwo))
+});
 
 //User 1 Random ID
 function uniqId() {
-  return Math.round(new Date().getTime() + (Math.random() * 100));
+  let id1 = Math.round(new Date().getTime() + (Math.random() * 100));
+  let id2 = Math.round(new Date().getTime() + (Math.random() * 100));
+  return id1;
+  return id2;
+  console.log(id1, id2);
 }
 
-var $li = $('<li/>', {
-  'class': 'bigger',
-  id: uniqId()
+$(function() {
+  let newID = document.querySelectorAll('.table__cell--short');
+
+  newID = {
+    id: uniqId()
+  };
+
 });
 
-var $span = $('<span/>', {
-  'class': 'Sp icon icon-color icon-plus',
-  title: 'Remove from list',
-  text: 'I\'m a span',
-  click: function() {
-    alert( $(this).parent().attr('id') );
-  }
-});
 
-$('body').append( $li.append( $span ) );
+// var $li = $('<li/>', {
+//   'class': 'bigger',
+//   id: uniqId()
+// });
+//
+// var $span = $('<span/>', {
+//   'class': 'Sp icon icon-color icon-plus',
+//   title: 'Remove from list',
+//   text: 'I\'m a span',
+//   click: function() {
+//     alert( $(this).parent().attr('id') );
+//   }
+// });
+//
+// $('body').append( $li.append( $span ) );
