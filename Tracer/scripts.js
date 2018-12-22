f537e3b011ffdd975df9e6d4c8adecc3c5446960
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 10:10:28 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,5 +1,26 @@
 $(function() {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  fetch('https://jsonplaceholder.typicode.com/albums/1')
     .then(response => response.json())
     .then(json => console.log(json))
 });
+
+//User 1 Random ID
+function uniqId() {
+  return Math.round(new Date().getTime() + (Math.random() * 100));
+}
+
+var $li = $('<li/>', {
+  'class': 'bigger',
+  id: uniqId()
+});
+
+var $span = $('<span/>', {
+  'class': 'Sp icon icon-color icon-plus',
+  title: 'Remove from list',
+  text: 'I\'m a span',
+  click: function() {
+    alert( $(this).parent().attr('id') );
+  }
+});
+
+$('body').append( $li.append( $span ) );
