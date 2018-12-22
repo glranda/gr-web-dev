fe01e02f0e30c34dd8d31923af8bffa7f6a2e329
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 17:28:43 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -66,7 +66,7 @@ function doSomething() {
     let container = $(".table-one");
     let ids = $(".user-id1");
     let albums = $(".album-name1");
-    container.remove( ".new-row" );
+    $( ".new-row" ).remove();
 
     for(var i = 0; i < data1.length; i++) {
       let newRow = '<div draggable="true" ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)" class="table__row new-row"><div class="user-id1 table__cell table__cell--short"></div><div class="album-name1 table__cell"></div></div>';
