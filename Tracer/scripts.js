a66356e5e0fb3c8916414089d892f67e871e55ca
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 13:33:13 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -12,9 +12,13 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
     let newRow = '<div class="table__row"><div class="user-id table__cell table__cell--short"></div><div class="album-name table__cell"></div></div>';
     container.after(newRow);
 
+    let ids = $(".user-id");
+    let idArray = [];
+    idArray.push(ids);
+    console.log(idArray)
   }
 
-  let ids = $(".user-id");
+
   console.log(ids.length)
   for(var i = 0; i < ids.length; i++){
     ids.text(data2[i].id);
