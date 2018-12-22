9b50ce301dded4c7342f815df8a418c8d1b8d15f
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 12:46:13 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -23,10 +23,10 @@ $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
 
   for(var i = 0; i < data2.length; i++){
 
-    let newRow = '<div class="table__row"></div>';
-    let shortCell = '<div class="table__cell table__cell--short"></div>';
+    let newRow = '<div class="table__row"><div class="table__cell table__cell--short"></div></div>';
+    //let shortCell = '<div class="table__cell table__cell--short"></div>';
 
-    container.after(newRow).append(shortCell);
+    container.after(newRow);
   }
 
 });
