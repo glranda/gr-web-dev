9c1a02e1df2acef7b2468ea9d88e120ac5f2e4a5
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 14:00:48 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -4,20 +4,32 @@
 //     .then(jsonOne => console.log(jsonOne))
 // });
 
-$.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
+$.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
   var container = $(".table-one");
 
-  for(var i = 0; i < data2.length; i++) {
-    let newRow = '<div class="table__row"><div class="user-id table__cell table__cell--short"></div><div class="album-name table__cell"></div></div>';
+  for(var i = 0; i < data1.length; i++) {
+    let newRow = '<div class="table__row"><div class="user-id1 table__cell table__cell--short"></div><div class="album-name1 table__cell"></div></div>';
     container.after(newRow);
   }
 
-  let ids = $(".user-id");
-  console.log(ids);
-  console.log(ids.length);
-  for(var j = 0; j < ids.length; j++) {
+  let albums = $(".album-name1");
+  for(var j = 0; j < albums.length; j++) {
+    albums[j].innerHTML = data1[j].title;
+  }
 
-    ids[j].innerHTML = data2[j].id;
+});
+
+$.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
+  var container = $(".table-two");
+
+  for(var i = 0; i < data2.length; i++) {
+    let newRow = '<div class="table__row"><div class="user-id2 table__cell table__cell--short"></div><div class="album-name2 table__cell"></div></div>';
+    container.after(newRow);
+  }
+
+  let albums = $(".album-name2");
+  for(var j = 0; j < albums.length; j++) {
+    albums[j].innerHTML = data2[j].title;
   }
 
 });
