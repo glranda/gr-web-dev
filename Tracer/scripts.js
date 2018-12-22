eb5e0d995a3b2accc1491196e0801ef23e9fc227
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 12:10:04 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -12,13 +12,20 @@
 $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
   var container = $(".table-one")[0];
 
+  // for(var i = 0; i < data2.length; i++){
+  //   var newDiv1 = document.createElement('div');
+  //   var newDiv2 = document.createElement('div');
+  //   newDiv1.className = 'table__cell table__cell--short';
+  //   newDiv2.className = 'table__cell table__cell';
+  //   container.append(newDiv1);
+  //   container.append(newDiv2);
+  // }
+
   for(var i = 0; i < data2.length; i++){
-    var newDiv1 = document.createElement('div');
-    var newDiv2 = document.createElement('div');
-    newDiv1.className = 'table__cell table__cell--short';
-    newDiv2.className = 'table__cell table__cell';
-    container.append(newDiv1);
-    container.append(newDiv2);
+    container.after("<div class='table__row'>" +
+                        "<div class='table__cell table__cell--short'></div>" +
+                        "<div class='table__cell table__cell'></div>" +
+                      "</div>");
   }
 
 });
