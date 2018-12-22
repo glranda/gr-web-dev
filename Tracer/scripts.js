1b4ccfeac8e2eaefc0c9d88c2c90bc47bdd3c63a
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 11:19:04 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,17 +1,18 @@
-$(function() {
-  fetch('https://jsonplaceholder.typicode.com/albums')
-    .then(responseOne => responseOne.json())
-    .then(jsonOne => console.log(jsonOne))
-});
+// $(function() {
+//   fetch('https://jsonplaceholder.typicode.com/albums')
+//     .then(responseOne => responseOne.json())
+//     .then(jsonOne => console.log(jsonOne))
+// });
 
-$.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
-  console.log(data1);
-  $('.user-one-id').html(data1.id);
-  //$('.user-one-title').html(data1.title);
-});
+// $.get('https://jsonplaceholder.typicode.com/albums?userId=1', function(data1) {
+//   console.log(data1);
+//
+// });
 
 $.get('https://jsonplaceholder.typicode.com/albums?userId=2', function(data2) {
-  console.log(data2);
-  $('.user-two-id').html(data2.title);
-  //$('.user-two-title').html(data2.title);
+  console.log(data2.length);
+
+  // for(var i = 0; i < data2.length; i++){
+  //   buttons[i].classList.add('hidden');
+  // }
 });
