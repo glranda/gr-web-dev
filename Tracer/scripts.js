a7af8fdcea75e81d5d107cbd63c4a8545458c9ab
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 10:04:42 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,7 +1,5 @@
 $(function() {
-    console.log('ready!');
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
 });
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
