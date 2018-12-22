38680931be18f092bd9b23805187eb94b1348918
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 11:07:01 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,8 +1,8 @@
-// $(function() {
-//   fetch('https://jsonplaceholder.typicode.com/albums/1')
-//     .then(responseOne => responseOne.json())
-//     .then(jsonOne => console.log(jsonOne))
-// });
+$(function() {
+  fetch('https://jsonplaceholder.typicode.com/albums')
+    .then(responseOne => responseOne.json())
+    .then(jsonOne => console.log(jsonOne))
+});
 
 $.get('https://jsonplaceholder.typicode.com/albums/1', function(data1) {
   console.log(data1);
@@ -13,22 +13,5 @@ $.get('https://jsonplaceholder.typicode.com/albums/1', function(data1) {
 $.get('https://jsonplaceholder.typicode.com/albums/2', function(data2) {
   console.log(data2);
   $('.user-two-id').html(data2.id);
-  $('.user-two-title').html(data2.id);
+  $('.user-two-title').html(data2.title);
 });
-
-
-// var $li = $('<li/>', {
-//   'class': 'bigger',
-//   id: uniqId()
-// });
-//
-// var $span = $('<span/>', {
-//   'class': 'Sp icon icon-color icon-plus',
-//   title: 'Remove from list',
-//   text: 'I\'m a span',
-//   click: function() {
-//     alert( $(this).parent().attr('id') );
-//   }
-// });
-//
-// $('body').append( $li.append( $span ) );
