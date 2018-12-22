36b2fd24c476ff5eb82dca691c2f3f9d1b9b0b6f
graysonrandall <40443785+graysonrandall@users.noreply.github.com>
2018-12-22 10:50:52 -0600

    Update scripts.js

--- Tracer/scripts.js
+++ Tracer/scripts.js
@@ -1,7 +1,28 @@
-$(function(data) {
-  fetch('https://jsonplaceholder.typicode.com/albums/1')
-    .then(responseOne => responseOne.json())
-    .then(jsonOne => console.log(jsonOne))
+// $(function(data) {
+//   fetch('https://jsonplaceholder.typicode.com/albums/1')
+//     .then(responseOne => responseOne.json())
+//     .then(jsonOne => console.log(jsonOne))
+//
+//     $('.test-id').html(data);
+// });
 
-    $('.test-id').html(data);
+$.get('https://jsonplaceholder.typicode.com/albums/1', function(data) {
+  $('.test-id').html(data.id);
 });
+
+
+// var $li = $('<li/>', {
+//   'class': 'bigger',
+//   id: uniqId()
+// });
+//
+// var $span = $('<span/>', {
+//   'class': 'Sp icon icon-color icon-plus',
+//   title: 'Remove from list',
+//   text: 'I\'m a span',
+//   click: function() {
+//     alert( $(this).parent().attr('id') );
+//   }
+// });
+//
+// $('body').append( $li.append( $span ) );
