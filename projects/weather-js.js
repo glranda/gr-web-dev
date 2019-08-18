ee2161de51b49a57bd115630b4dcb6736a72e759
Grayson <graysonrandallwebdev@gmail.com>
2019-08-18 17:30:56 -0400

    icons path

--- projects/weather-js.js
+++ projects/weather-js.js
@@ -71,13 +71,16 @@
 			console.log(data.cities[city].current[0].condition);
 			if (hours >= 20 || hours <= 7 && data.cities[city].current[0].condition !== 'Cloudy') {
 				body.classList.add('darkmode');
-				body.classList.remove('day', 'cloudy');
+				body.classList.remove('day');
+				body.classList.remove('cloudy');
 			} else if (hours <= 20 && hours >= 7 && data.cities[city].current[0].condition == 'Cloudy') {
 				body.classList.add('cloudy');
-				body.classList.remove('darkmode', 'day');
+				body.classList.remove('darkmode');
+				body.classList.remove('day');
 			} else if (hours <= 20 && hours >= 7 && data.cities[city].current[0].condition !== 'Cloudy') {
 				body.classList.add('day');
-				body.classList.remove('darkmode', 'cloudy');
+				body.classList.remove('darkmode');
+				body.classList.remove('cloudy');
 			}
 
 			let temp = '<p>' + 'It is ' + '<strong>' + data.cities[city].current[0].temp + '&deg;' + '</strong>' + ' outside now' + '</p>';
